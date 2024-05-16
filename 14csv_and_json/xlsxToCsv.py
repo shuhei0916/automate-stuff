1e1909ca4181071738ac0819703e6571d37042ba
shuhei0916 <shuheiito.stc@gmail.com>
2024-05-16 14:22:44 +0900

    skipped xlsxToCsv project at the end of chapter 14

---
+++ 14csv_and_json/xlsxToCsv.py
@@ -0,0 +1,8 @@
+# import openpyxl
+# import os
+
+# for excel_file in os.listdir('.'):
+#     # xlsxファイルでなければスキップ
+#     for sheet_name in wb.get_sheet_names():
+#         # ワークブックのシートをループする
+#         sheet = wb.get
